89f3a4a61029d1d9af809ef7a65773b95f7ba3a4
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-10-02 00:12:52 -0400

    create and render stores from api

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const endPoint = "http://localhost:3000/api/v1/stores"
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('Loaded')
+	getStores()
+})
+
+function getStores() {
+	fetch(endPoint)
+	.then(res => res.json())
+	.then(stores => {
+		console.log(stores)
+		stores.data.forEach(store => {
+			let newStore = new Store(store, store.attributes)
+
+			document.querySelector('#stores-list').innerHTML += newStore.renderStoreList()
+		})
+	})
+}
