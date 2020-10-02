a13a55b03c97d656d8f19314502b86eac124abbb
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-10-02 06:17:29 -0400

    edit styles and initial page layouts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ const endPoint = "http://localhost:3000/api/v1/stores"
 document.addEventListener('DOMContentLoaded', () => {
 	console.log('Loaded')
 	getStores()
+	window.onscroll = function() {myFunction()};
+
+	// Get the navbar
+	var header = document.querySelector("header");
+
+	// Get the offset position of the navbar
+	var sticky = header.offsetTop;
+
+	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function myFunction() {
+	  if (window.pageYOffset >= sticky) {
+	    header.classList.add("sticky")
+	  } else {
+	    header.classList.remove("sticky");
+	  }
+	}
 })
 
 function getStores() {
@@ -13,7 +29,11 @@ function getStores() {
 		stores.data.forEach(store => {
 			let newStore = new Store(store, store.attributes)
 
-			document.querySelector('.sidenav').innerHTML += newStore.renderStoreList()
+			document.querySelector('.stores-container').innerHTML += newStore.renderStoreList()
 		})
 	})
+}
+
+function stickyNavbar() {
+
 }
